979b6d760247844ba53862a3d78d53518c14697f
trungdang97 <35669562+trungdang97@users.noreply.github.com>
2018-04-24 21:28:23 +0700

    init

---
+++ Dicho_online/Dicho_online.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dicho_online.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
