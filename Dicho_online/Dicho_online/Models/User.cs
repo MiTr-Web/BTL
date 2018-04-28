ed38d35d90e307a4764a5d2f6c6fb2b47a8edef9
trungdang97 <35669562+trungdang97@users.noreply.github.com>
2018-04-28 12:17:13 +0700

    giao dien login va register

---
+++ Dicho_online/Dicho_online/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dicho_online.Models
+{
+    public class User
+    {
+        string email;
+        string userid;
+    }
+}
