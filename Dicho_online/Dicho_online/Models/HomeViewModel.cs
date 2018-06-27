44e13ccdbd72deb74263a0aa023c14db2c0b53a8
trungdang97 <35669562+trungdang97@users.noreply.github.com>
2018-06-27 09:21:08 +0700

    Revert "add some ignore files"
    This reverts commit ffbf5e6d88c6e8bac4f646487575ef4a1d73f8dd.

--- Dicho_online/Dicho_online/Models/HomeViewModel.cs
+++ Dicho_online/Dicho_online/Models/HomeViewModel.cs
@@ -11,5 +11,6 @@ namespace Dicho_online.Models
         public IPagedList<Product> productList { get; set; }
         public IEnumerable<Product> product_detail { get; set; }
         public IEnumerable<string> priceSort { get; set; }
+        public IEnumerable<Product> result { get; set; }
     }
 }
